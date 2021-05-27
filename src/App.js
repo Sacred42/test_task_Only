42b5721dc68a9de97b5be0d15788889319da1c66
Said <sacred42rus@mail.ru>
2021-05-27 15:29:38 +0700

    is done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Auth from './components/Auth/Auth';
 import SuccessAuth from './components/SuccessAuth/SuccessAuth';
 import Header from './components/Header/Header';
+import { Redirect } from "react-router-dom"
 import { BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
@@ -10,8 +11,9 @@ function App() {
     <div className="App">
       <Header/>
        <div className="container">
-         <Route exact path ="/" component={Auth}/>
+         <Route exact path ="/"  render={()=><Redirect to="/login"/>}/>
          <Route path ="/profile" component={SuccessAuth}/>
+         <Route exact path ="/login" component={Auth} />
       </div>  
     </div>
     </BrowserRouter>
